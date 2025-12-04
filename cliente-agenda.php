b0290afb96bcfaa64497356659120ac1f4f3df2f
aprendizerick762 <aprendiz.erickdossantos@gmail.com>
2025-12-03 22:44:36 -0300

    Versão apresentada 1.1

--- cliente-agenda.php
+++ cliente-agenda.php
@@ -118,7 +118,21 @@ if ($stmt = mysqli_prepare($connect, $sql)) {
 
 <body>
 
-<?php include_once 'inc/header.php'; ?>
+<header class="navbar">
+  <div class="logo">
+    <img src="img/LogoNailspotofc.png" alt="Logo NailSpot" />
+    <span class="nome-logo">NailSpot</span>
+  </div>
+
+  <nav>
+    <ul>
+      <li><a href="cliente-servicos.php">Serviços</a></li>
+      <li><a href="cliente-agenda.php" class="ativo">Agenda</a></li>
+      <li><a href="cliente-perfil.php">Perfil</a></li>
+      <li><a href="login.php">Sair</a></li>
+    </ul>
+  </nav>
+</header>
 
 <h1>Minha Agenda</h1>
 
